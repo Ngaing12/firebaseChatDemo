d9893a7fcf2fa2e94262f0dc6f68b6bb0d964853
Illia Rahavoi <irahavoi@gmail.com>
2016-07-08 12:56:42 -0500

    Up

--- web/js/app.js
+++ web/js/app.js
@@ -1,19 +1,18 @@
 // Initialize Firebase
 var config = {
-apiKey: "AIzaSyDhAhu5a_lVOtRkxfZEJDz4umKziUoadik",
-authDomain: "qrioscat-248db.firebaseapp.com",
-databaseURL: "https://qrioscat-248db.firebaseio.com",
-storageBucket: "qrioscat-248db.appspot.com",
+apiKey: "PUT_YOUR_API_KEY_HERE",
+authDomain: "PUTH_YOUR_AUTH_DOMAIN_HERE",
+databaseURL: "PUT_YOUR_DB_URL_HERE",
+storageBucket: "PUT_YOUR_STORAGE_BUCKET_HERE",
 };
-
 var messagesList = document.getElementById("messages"),
 newMsg = document.getElementById("newMsg"),
 msgContainer = document.getElementById("messagesContainer"),
 userId = document.getElementById("userId");
 
 firebase.initializeApp(config);
-firebase.database().ref('messages').on('child_added', function(snapshot, prevKey){
-handleNewMsg(snapshot);
+	firebase.database().ref('messages').on('child_added', function(snapshot, prevKey){
+	handleNewMsg(snapshot);
 });
 
 newMsg.onkeyup = function(evt) {
@@ -26,21 +25,21 @@ newMsg.onkeyup = function(evt) {
 
 
 function handleSendNewMsg(){
- chat(newMsg.value, userId.value);
+	chat(newMsg.value, userId.value);
 }
 
 function chat(msg, userId){
-firebase.database().ref('messages')
-    .push({
-        message : msg,
-        date : new Date(),
-        author: userId
-    });
+	firebase.database().ref('messages')
+	    .push({
+	        message : msg,
+	        date : new Date(),
+	        author: userId
+	    });
 }
 
 function handleNewMsg(snapshot){
-appendMessage(snapshot.val());
-msgContainer.scrollTop = msgContainer.scrollHeight;
+	appendMessage(snapshot.val());
+	msgContainer.scrollTop = msgContainer.scrollHeight;
 }
 
 function appendMessage(message) {
